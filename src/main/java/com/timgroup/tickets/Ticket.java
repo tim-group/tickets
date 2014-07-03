5e0f65620074d2a1812e17bf917951416f15a478
Steve Haslam <steve.haslam@timgroup.com>
2014-07-03 12:47:33 +0100

    Basic marshalling and unmarshalling of ticket data

--- src/main/java/com/timgroup/tickets/Ticket.java
+++ src/main/java/com/timgroup/tickets/Ticket.java
@@ -1,14 +1,38 @@
 package com.timgroup.tickets;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Ticket {
     private final TicketMacGenerator macGenerator;
+    private final Map<Character, List<String>> data = new LinkedHashMap<Character, List<String>>();
 
     public Ticket(TicketMacGenerator macGenerator) {
         this.macGenerator = macGenerator;
     }
 
     public String marshal() {
-        return "x" + macGenerator.generateMAC("");
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<Character, List<String>> e : data.entrySet()) {
+            for (String value : e.getValue()) {
+                if (builder.length() > 0) {
+                    builder.append(',');
+                }
+                builder.append(e.getKey().charValue());
+                builder.append(value);
+            }
+        }
+        String payload = builder.toString();
+        if (!payload.isEmpty()) {
+            builder.append(',');
+        }
+        builder.append('x').append(macGenerator.generateMAC(payload));
+        return builder.toString();
     }
 
     public void unmarshal(String input) throws InvalidTicketException {
@@ -28,5 +52,65 @@ public class Ticket {
         if (!ourMac.equals(theirMac)) {
             throw new TicketMacMismatchException(ourMac, theirMac);
         }
+        data.clear();
+        for (String part : Arrays.asList(parts)) {
+            if (part.isEmpty()) {
+                continue;
+            }
+            char key = part.charAt(0);
+            if (key == 'x') {
+                continue;
+            }
+            String value = part.substring(1);
+            add(key, value);
+        }
+    }
+
+    public List<String> get(char key) {
+        if (!data.containsKey(key)) {
+            return Collections.emptyList();
+        }
+        return data.get(key);
+    }
+
+    public String getOne(char key) {
+        if (!data.containsKey(key)) {
+            return null;
+        }
+        List<String> list = data.get(key);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    public Set<Character> keySet() {
+        return data.keySet();
+    }
+
+    public void add(char key, String value) {
+        if (key == 'x') {
+            throw new IllegalArgumentException("key 'x' is reserved for the MAC");
+        }
+        List<String> list = data.get(key);
+        if (list == null) {
+            list = new ArrayList<String>();
+            data.put(key, list);
+        }
+        list.add(value);
+    }
+
+    public void set(char key, String value) {
+        if (key == 'x') {
+            throw new IllegalArgumentException("key 'x' is reserved for the MAC");
+        }
+        data.put(key, new ArrayList<String>(Arrays.asList(value)));
+    }
+
+    public void set(char key, List<String> value) {
+        if (key == 'x') {
+            throw new IllegalArgumentException("key 'x' is reserved for the MAC");
+        }
+        data.put(key, new ArrayList<String>(value));
     }
 }
--- src/test/java/com/timgroup/tickets/TicketTest.java
+++ src/test/java/com/timgroup/tickets/TicketTest.java
@@ -1,13 +1,19 @@
 package com.timgroup.tickets;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.junit.Test;
 
-import com.timgroup.tickets.InvalidTicketException;
-import com.timgroup.tickets.Ticket;
-import com.timgroup.tickets.TicketMacGenerator;
-import com.timgroup.tickets.TicketMacMismatchException;
-
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 
 public class TicketTest {
     private static final TicketMacGenerator DUMMY_MAC_GENERATOR = new TicketMacGenerator() {
@@ -21,14 +27,74 @@ public class TicketTest {
         assertEquals("xnoodles", ticket.marshal());
     }
 
+    @Test public void marshals_single_attribute_with_empty_value() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.add('a', "");
+        assertEquals("a,xnoodles", ticket.marshal());
+    }
+
+    @Test public void marshals_single_attribute_with_value() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.add('a', "1");
+        assertEquals("a1,xnoodles", ticket.marshal());
+    }
+
+    @Test public void marshals_two_attributes_in_insertion_order() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.add('b', "2");
+        ticket.add('a', "1");
+        assertEquals("b2,a1,xnoodles", ticket.marshal());
+    }
+
+    @Test public void marshals_multiple_attribute_values() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.add('a', "1");
+        ticket.add('a', "2");
+        assertEquals("a1,a2,xnoodles", ticket.marshal());
+    }
+
+    @Test public void marshals_attribute_values_grouped_together() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.add('a', "1");
+        ticket.add('b', "1");
+        ticket.add('a', "2");
+        assertEquals("a1,a2,b1,xnoodles", ticket.marshal());
+    }
+
     @Test public void unmarshals_just_mac_to_empty_ticket() throws Exception {
         Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
         ticket.unmarshal("xnoodles");
+        assertThat(ticket, is(emptyTicket()));
     }
 
-    @Test public void unmarshals_single_attribute() throws Exception {
+    @Test public void unmarshals_single_attribute_with_empty_value() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.unmarshal("a,xnoodles");
+        assertThat(ticket, is(ticket().containing('a', "")));
+    }
+
+    @Test public void unmarshals_single_attribute_with_value() throws Exception {
         Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
         ticket.unmarshal("a1,xnoodles");
+        assertThat(ticket, is(ticket().containing('a', "1")));
+    }
+
+    @Test public void unmarshals_two_attributes_in_order() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.unmarshal("a1,b1,xnoodles");
+        assertThat(ticket, is(ticket().containing('a', "1").containing('b', "1").inOrder()));
+    }
+
+    @Test public void unmarshals_attributes_even_if_not_grouped_correctly() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.unmarshal("a1,b1,a2,xnoodles");
+        assertThat(ticket, is(ticket().containing('a', "1", "2").containing('b', "1")));
+    }
+
+    @Test public void unmarshalling_ignores_empty_parts() throws Exception {
+        Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
+        ticket.unmarshal(",xnoodles");
+        assertThat(ticket, is(emptyTicket()));
     }
 
     @Test(expected = InvalidTicketException.class) public void fails_to_unmarshal_empty_string() throws Exception {
@@ -44,5 +110,62 @@ public class TicketTest {
     @Test(expected = TicketMacMismatchException.class) public void fails_to_unmarshal_attribute_and_invalid_mac() throws Exception {
         Ticket ticket = new Ticket(DUMMY_MAC_GENERATOR);
         ticket.unmarshal("a1,xblahblah");
+    }
+
+    private static Matcher<Ticket> emptyTicket() {
+        return new TypeSafeDiagnosingMatcher<Ticket>(Ticket.class) {
+            @Override protected boolean matchesSafely(Ticket item, Description mismatchDescription) {
+                if (!item.keySet().isEmpty()) {
+                    mismatchDescription.appendText("contains keys ").appendValue(item.keySet());
+                    return false;
+                }
+                return true;
+            }
+
+            @Override public void describeTo(Description description) {
+                description.appendText("empty ticket");
+            }
+        };
+    }
+
+    private static TicketWithDataMatcher ticket() {
+        return new TicketWithDataMatcher();
+    }
+
+    private static class TicketWithDataMatcher extends TypeSafeDiagnosingMatcher<Ticket> {
+        private final Map<Character, List<String>> expectedData = new LinkedHashMap<Character, List<String>>();
+        private boolean strictKeyOrder = true;
+
+        public TicketWithDataMatcher() {
+            super(Ticket.class);
+        }
+
+        @Override protected boolean matchesSafely(Ticket item, Description mismatchDescription) {
+            if (!item.keySet().equals(expectedData.keySet())) {
+                mismatchDescription.appendText("ticket contains keys ").appendValue(item.keySet()).appendText(" expected ")
+                        .appendValue(expectedData.keySet());
+                return false;
+            }
+            if (strictKeyOrder && !new ArrayList<Character>(item.keySet()).equals(new ArrayList<Character>(expectedData.keySet()))) {
+                mismatchDescription.appendText("ticket keys in order ").appendValue(item.keySet()).appendText(" expected ")
+                        .appendValue(expectedData.keySet());
+                return false;
+            }
+            return true;
+        }
+
+        @Override public void describeTo(Description description) {
+            description.appendText("ticket containining ").appendValue(expectedData);
+        }
+
+        public TicketWithDataMatcher containing(char key, String... value) {
+            expectedData.put(key, Arrays.asList(value));
+            return this;
+        }
+
+        public TicketWithDataMatcher inOrder() {
+            strictKeyOrder = true;
+            return this;
+        }
     }
 }
