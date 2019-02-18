1b1bbb7a96df225110b249f12edae5cdb2c90ab8
Steve Haslam <steve.haslam@timgroup.com>
2019-02-18 15:23:32 +0000

    SteveH: upgrade Mockito to 2.24 [Java11]

--- src/test/java/com/timgroup/tickets/TicketFactoryTest.java
+++ src/test/java/com/timgroup/tickets/TicketFactoryTest.java
@@ -3,7 +3,7 @@ package com.timgroup.tickets;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
